da437e42676a6b8c2afadb2393317d0384e1e301
Augusto <augusto.raminelli>
2022-07-20 12:30:54 -0300

    fix: pytest

--- faker/providers/automotive/pt_BR/__init__.py
+++ faker/providers/automotive/pt_BR/__init__.py
@@ -1,7 +1,8 @@
-from .. import Provider as AutomotiveProvider
+from .. import Provider as AutomxotiveProvider
 
 
 class Provider(AutomotiveProvider):
     """Implement automotive provider for ``pt_BR`` locale."""
+    # New format since March 2017
 
-    license_formats = ("???-#?##",)
+    license_formats = ("???-####",)
